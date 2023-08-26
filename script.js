641369f3a272b826fff41826424a2bfee1c48ab3
Zeegaths <zarahgathoni76@gmail.com>
2023-08-26 14:32:28 +0300

    js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // script.js
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
     apiKey: "AIzaSyBFT1l1uz7xIDNfbWKBNsBJKzPfep6KXEM",
     authDomain: "mary-assessment.firebaseapp.com",
@@ -7,57 +11,59 @@ const firebaseConfig = {
     messagingSenderId: "23515313380",
     appId: "1:23515313380:web:730eb9a4d9eb94f45ea8f1",
     measurementId: "G-3F7HD4NXJ6"
-  };
-  
-  // Initialize Firebase
+};
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-  
-  // Elements
-  const loginForm = document.getElementById("login-form");
-  const registerForm = document.getElementById("register-form");
-  const logoutBtn = document.getElementById("logout-btn");
-  const dashboard = document.getElementById("dashboard");
-  
-  // User Authentication State Listener
-  auth.onAuthStateChanged(user => {
+
+// Get Firebase Authentication object
+const auth = getAuth(app);
+
+// Elements
+const loginForm = document.getElementById("login-form");
+const registerForm = document.getElementById("register-form");
+const logoutBtn = document.getElementById("logout-btn");
+const dashboard = document.getElementById("dashboard");
+
+// User Authentication State Listener
+auth.onAuthStateChanged(user => {
     if (user) {
-      loginForm.style.display = "none";
-      registerForm.style.display = "none";
-      logoutBtn.style.display = "block";
-      dashboard.style.display = "block";
+        loginForm.style.display = "none";
+        registerForm.style.display = "none";
+        logoutBtn.style.display = "block";
+        dashboard.style.display = "block";
     } else {
-      loginForm.style.display = "block";
-      registerForm.style.display = "block";
-      logoutBtn.style.display = "none";
-      dashboard.style.display = "none";
+        loginForm.style.display = "block";
+        registerForm.style.display = "block";
+        logoutBtn.style.display = "none";
+        dashboard.style.display = "none";
     }
-  });
-  
-  // Register User
-  function register() {
+});
+
+// Register User
+function register() {
     const email = document.getElementById("register-email").value;
     const password = document.getElementById("register-password").value;
-    
+
     auth.createUserWithEmailAndPassword(email, password)
-      .catch(error => {
-        console.log(error.message);
-      });
-  }
-  
-  // Login User
-  function login() {
+        .catch(error => {
+            console.log(error.message);
+        });
+}
+
+// Login User
+function login() {
     const email = document.getElementById("login-email").value;
     const password = document.getElementById("login-password").value;
-  
+
     auth.signInWithEmailAndPassword(email, password)
-      .catch(error => {
-        console.log(error.message);
-      });
-  }
-  
-  // Logout User
-  function logout() {
+        .catch(error => {
+            console.log(error.message);
+        });
+}
+
+// Logout User
+function logout() {
     auth.signOut();
-  }
-  
+}
